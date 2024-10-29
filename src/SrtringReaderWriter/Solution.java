9c3a5ec368b2d4ec7cf36a518b7ddf4c9dd801b6
d.dovgal <ddovgal@hotmail.com>
2024-10-29 14:36:53 +0300

    String -> Reader, Writer -> String

--- src/SrtringReaderWriter/Solution.java
+++ src/SrtringReaderWriter/Solution.java
@@ -1,12 +1,12 @@
 package SrtringReaderWriter;
 
 import java.io.*;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        String secretRecipe = "You very important string - на разных языках."; // different languages
-        ByteArrayInputStream bais = new ByteArrayInputStream(secretRecipe.getBytes(Charset.forName("UTF-8"))); // encoding is important!!!
+        String secretRecipe = "Your very important string - на разных языках."; // different languages
+        ByteArrayInputStream bais = new ByteArrayInputStream(secretRecipe.getBytes(StandardCharsets.UTF_8)); // encoding is important!!!
         InputStreamReader reader = new InputStreamReader(bais);
 
         // reader is ready so, let's create writer, read a string and sout it.
